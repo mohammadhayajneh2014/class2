41abc3fd223708433e34e5391e61b71a70cd03f6
mohammad ahmad theeb hayajneh <mohammadhayagneh96@gmail.com>
2021-06-07 17:53:40 +0300

    lab22

---
+++ src/component/footer.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className='footer'>
+        <h3>&copy; Mohammad hayajneh</h3>
+      </div>
+    );
+  }
+}
+export default Footer;
